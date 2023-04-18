7a6be1a5c8bc0164d869d6aae69696eb67a1a9c6
Bhavya Wahie <bhavyawahie@gmail.com>
2023-04-18 22:37:40 +0530

    exceptions now handled using exceptionHandler controller

--- src/main/java/com/kmh/taskmanager/controllers/TaskController.java
+++ src/main/java/com/kmh/taskmanager/controllers/TaskController.java
@@ -1,12 +1,15 @@
 package com.kmh.taskmanager.controllers;
 
+import java.text.ParseException;
 import java.util.List;
 
 import com.kmh.taskmanager.Services.TaskService;
 import com.kmh.taskmanager.dto.CreateTaskDTO;
+import com.kmh.taskmanager.dto.ErrorResponseDTO;
 import com.kmh.taskmanager.entities.TaskEntity;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -39,8 +42,17 @@ public class TaskController {
     }
 
     @PostMapping("")
-    public ResponseEntity<TaskEntity> addTask(@RequestBody CreateTaskDTO body) {
+    public ResponseEntity<TaskEntity> addTask(@RequestBody CreateTaskDTO body) throws ParseException {
         var task = taskService.addTask(body.getTitle(), body.getDescription(), body.getDeadline());
         return ResponseEntity.ok(task);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDTO> handleErrors(Exception e) {
+        if(e instanceof ParseException){
+            return ResponseEntity.badRequest().body(new ErrorResponseDTO("Invalid Date Format"));
+        }
+        e.printStackTrace();
+        return ResponseEntity.internalServerError().body(new ErrorResponseDTO("Something went wrong, but don't fret!"));
     }
 }
