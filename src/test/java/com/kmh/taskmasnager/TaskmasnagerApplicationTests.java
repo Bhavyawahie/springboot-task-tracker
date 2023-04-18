db3f01bc0ea6a63f7b9871027beb076fec8bb900
Bhavya Wahie <bhavyawahie@gmail.com>
2023-04-18 16:19:10 +0530

    folder structure for api project added

---
+++ src/test/java/com/kmh/taskmasnager/TaskmasnagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kmh.taskmasnager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmasnagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
